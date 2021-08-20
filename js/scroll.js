f40fa7b072b344f3171d4e27e0c79995706b902c
Daulin <daulinri@gmail.com>
2021-08-19 19:52:56 -0500

    se modifica logo y fuente

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () =>{
+        scrolled = window.scrollY;
+
+        if(scrolled){
+            navbar.classList.add('scroll')
+        }else{
+            navbar.classList.remove('scroll');
+        }
+
+        console.log(scrolled)
+
+});
